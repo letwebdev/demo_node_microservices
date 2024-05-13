6ad7c1f7e317bb23a7165c520c86740501506f34
let webDeveloper <letwebdev@qq.com>
2024-05-13 10:11:58 +0800

    merge

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,7 +1,10 @@
 import Koa from "koa"
+import logger from "koa-logger"
 import serve from "koa-static"
 import mount from "koa-mount"
+
 const app = new Koa()
+app.use(logger())
 
 app.use(mount("/staticServer", serve("services/staticServer")))
 app.listen(3000)
