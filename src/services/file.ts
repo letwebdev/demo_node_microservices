7ebd42e2992fa1124912ba058266071a87596934
anonymous <null@undefined.void>
2024-05-13 09:45:23 +0800

    use koa-mount

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,6 +1,7 @@
 import Koa from "koa"
 import serve from "koa-static"
+import mount from "koa-mount"
 const app = new Koa()
 
-app.use(serve("services/staticServer"))
+app.use(mount("/staticServer", serve("services/staticServer")))
 app.listen(3000)
