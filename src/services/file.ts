0334b75acbe39e45e7f32ebd67778c7a8d349bf4
anonymous <null@undefined.void>
2024-05-15 01:51:35 +0800

    install MongoDB

--- src/services/file.ts
+++ src/services/file.ts
@@ -3,8 +3,32 @@ import logger from "koa-logger"
 import serve from "koa-static"
 import mount from "koa-mount"
 
+import { MongoClient } from "mongodb"
+
 const app = new Koa()
 app.use(logger())
 
 app.use(mount("/staticServer", serve("services/staticServer")))
+
+const urlOfMongoDB = "mongodb://localhost:27017"
+const client = new MongoClient(urlOfMongoDB)
+const dbName = "myProject"
+
+async function main() {
+  await client.connect()
+  console.log("Connected successfully to server")
+  const db = client.db(dbName)
+  const collection = db.collection("documents")
+
+  const insertResult = await collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }])
+  console.log("Inserted documents =>", insertResult)
+
+  return "done."
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close())
+
 app.listen(3000)
